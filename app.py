10784ba48f3ccdf0c5ab1b8db4c234f2b79e5998
Kaito <gojgoj58@gmail.com>
2024-06-09 00:22:05 +0300

    Login-signUp_buttun-working

--- app.py
+++ app.py
@@ -1,34 +1,83 @@
-from flask import Flask, redirect,render_template, request,flash
+from flask import Flask, redirect, render_template, request, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newflask.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
 db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     text = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('posts', lazy=True))
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-@app.route("/index")
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route("/create", methods=['POST',"GET"])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password')
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if User.query.filter_by(username=username).first():
+            flash('Username already exists')
+            return redirect(url_for('signup'))
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for('index'))
+    return render_template('signup.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route("/create", methods=['POST', 'GET'])
+@login_required
 def create():
     if request.method == 'POST':
         title = request.form['title']
         text = request.form['text']
-        
+
         if not title or not text:
             flash('Title and text cannot be empty')
             return redirect('/create')
-        
-        post = Post(title=title, text=text)
+
+        post = Post(title=title, text=text, user_id=current_user.id)
 
         try:
             db.session.add(post)
@@ -41,15 +90,11 @@ def create():
     else:
         return render_template('create.html')
 
-@app.route("/reg")
-def registration():
-    return render_template('reg.html')
-
 
 @app.route("/posts")
 def posts():
     posts = Post.query.all()
-    return render_template('posts.html',posts=posts)
+    return render_template('posts.html', posts=posts)
 
 
 @app.route("/post/<int:post_id>")
@@ -58,13 +103,18 @@ def post_detail(post_id):
     return render_template('post_detail.html', post=post)
 
 
-
-
 @app.route("/about")
 def about():
     return render_template('about.html')
 
 
+@app.route("/index")
+@app.route('/')
+def index():
+    return render_template('index.html', title='Welcome to Movie Reviews')
+
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create database tables
     app.run(debug=True)
-    
