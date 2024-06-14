21e32e06520313954d76bf0056403579baff4893
Kaito <gojgoj58@gmail.com>
2024-06-14 23:26:54 +0300

    work?

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ login_manager.login_view = 'login'
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
